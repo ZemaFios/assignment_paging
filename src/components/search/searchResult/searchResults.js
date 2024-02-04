ce4ce0e4f0da09bd783b8bddc5e4bb8d7f2c8544
Søren Nielsen <123360946+ZemaFios@users.noreply.github.com>
2024-02-05 00:03:00 +0100

    Update

--- src/components/search/searchResult/searchResults.js
+++ src/components/search/searchResult/searchResults.js
@@ -1,25 +1,26 @@
+import Link from 'next/link';
 import styles from './searchResults.module.css';
 
-const SearchResults = ({results}) => {
-  
-    const resultItems = results.data;
+const SearchResults = ({ results }) => {
+  const resultItems = results.data;
 
-    return (
-
-        <div className={styles.container}>
-            {
-                resultItems?.map( (item, index) => {
-
-                    return <div className={styles.item} key={item._id}>
-                        <h3>{item.author}</h3>
-                        <p>Gallery: {item.gallery}</p>
-                        <p>Camera Model: {item.meta.image.Model}</p>
-                        <hr/>
-                    </div>
-
-                })
-            }
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      {resultItems?.map((item, index) => {
+        return (
+          <div className={styles.item} key={item._id}>
+            <h3>
+              <Link href={`/author/${item.author}`}>{item.author}</Link>
+            </h3>
+            <p>
+              Gallery: <Link href={`/gallery/${item.gallery}`}>{item.gallery}</Link>
+            </p>
+            <p>Camera Model: {item.meta.image.Model}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-export default SearchResults
+export default SearchResults;
